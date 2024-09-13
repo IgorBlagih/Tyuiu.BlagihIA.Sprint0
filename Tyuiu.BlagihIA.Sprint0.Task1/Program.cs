d7ad802c157e7c0c71215bb4c02f36ce181ac1b0
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-09-13 18:21:52 +0500

    Task1

---
+++ Tyuiu.BlagihIA.Sprint0.Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.BlagihIA.Sprint0.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Игорь";
+            int age = 17;
+            Console.WriteLine("Здраствуйте меня зовут " + name + ", мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
