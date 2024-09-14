bef1b7f1ac480fb0093d70a3bb70a6aed94fc4d8
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-09-14 17:12:54 +0500

    Task4

---
+++ Tyuiu.BlagihIA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BlagihIA.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.BlagihIA.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1,5));
+
+            Console.WriteLine(DataService.Substraction(10, 5));
+
+            Console.WriteLine(DataService.Multiplication(6, 7));
+            
+            Console.WriteLine(DataService.Division(64, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
