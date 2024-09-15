1ee3a5b49c601a24bca41f5fd3300277939e7379
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-09-15 14:45:45 +0500

    Task5

---
+++ Tyuiu.BlagihIA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BlagihIA.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.BlagihIA.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" A + B = " + DataService.Addition(3, 5));
+
+            Console.WriteLine("A - B = " + DataService.Substraction(10, 7));
+
+            Console.WriteLine("A * B = " + DataService.Multiplication(5, 8));
+
+            Console.WriteLine("A / B = " + DataService.Division(15, 0));
+
+            Console.ReadKey();
+        }
+    }
+}
